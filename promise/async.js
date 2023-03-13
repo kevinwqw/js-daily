6ee441b854a37b78339f7a6033da6ae6fc9260c8
KevinWang <kevinwang2020@sina.com>
2023-03-13 22:58:19 +0800

    Update async method

--- promise/async.js
+++ promise/async.js
@@ -2,7 +2,10 @@ const _async = (fn) => {
     return new Promise((resolve, reject) => {
         try {
             const res = fn();
-            if ((typeof res === 'object' && res !== null) || typeof res === 'function') {
+            if (
+                ((typeof res === 'object' && res !== null) || typeof res === 'function') &&
+                typeof res.then === 'function'
+            ) {
                 // 将fn的执行结果转换成promise对象
                 Promise.resolve(res).then(resolve, reject);
             } else {
