1cbad59163201f58c82fc2c5b7fae6f3f0da35e2
KevinWang <kevinwang2020@sina.com>
2023-03-14 16:01:38 +0800

    Add throttlePro method

--- utils/throttle.js
+++ utils/throttle.js
@@ -15,3 +15,34 @@ const throttle = (fn, delay) => {
         }, delay);
     };
 };
+
+/**
+ * @param {Function} fn
+ * @param {number} delay
+ * @return {Function}
+ */
+function throttlePro(fn, delay) {
+    let shouldwait = false;
+    let remainArgs = null;
+
+    const timeoutFunc = () => {
+        if (remainArgs) {
+            fn(remainArgs);
+            remainArgs = null;
+            setTimeout(timeoutFunc, delay);
+        } else {
+            shouldwait = false;
+        }
+    };
+
+    return (...args) => {
+        if (shouldwait) {
+            remainArgs = args;
+            return;
+        }
+        fn(...args);
+        shouldwait = true;
+
+        setTimeout(timeoutFunc, delay);
+    };
+}
